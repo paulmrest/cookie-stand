fa6d6e34d9d3e72e672ddae322b9f27806aae33f
paulmrest <paulmrest@gmail.com>
2020-05-05 14:18:24 -0700

    Updated to use an object constructor

--- JS/app.js
+++ JS/app.js
@@ -4,345 +4,79 @@
 const openingTime24Hr = 600; //6 am in 24 hour time
 const closingTime24Hr = 2000; //8 pm in 24 hour time
 
-//object literals
-var seattleStore = {
-  storeLocation: 'Seattle',
-  minCustPerHour: 23,
-  maxCustPerHour: 65,
-  avgCookiesPerSale: 6.3,
-  hoursOpen: new Array (), //stores the hours open in 24 hour format as integer values
-  customersPerHour: new Array(),
-  cookiesPurchPerHour: new Array(),
+//constructor
+function CookieStore(storeLocation, minCustPerHour, maxCustPerHour, avgCookiesPerSale) {
+  this.storeLocation = storeLocation;
+  this.minCustPerHour = minCustPerHour;
+  this.maxCustPerHour = maxCustPerHour;
+  this.avgCookiesPerSale = avgCookiesPerSale;
+  this.hoursOpen = new Array();
+  this.customersPerHour = new Array();
+  this.cookiesPurchPerHour = new Array();
+}
 
-  populateHoursOpen: function() {
-    const totalHoursOpen = Math.abs(closingTime24Hr - openingTime24Hr) / 100;
-    for (var i = 0; i <= totalHoursOpen; i++)
-    {
-      var hourTime = openingTime24Hr + (i * 100);
-      this.hoursOpen.push(hourTime);
-    }
-  },
-
-  simulateCustPerHour: function() {
-    //populate hours open
-    this.populateHoursOpen();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      var customersInOneHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-      this.customersPerHour.push(customersInOneHour);
-    }
-  },
-
-  simulateCookiesPerHour: function() {
-    //populate customersPerHour
-    this.simulateCustPerHour();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      this.cookiesPurchPerHour.push(this.customersPerHour[i] * this.avgCookiesPerSale);
-    }
-  },
-
-  renderAsList: function() {
-    var totalCookieSales = 0;
-    var salesListsDiv = document.getElementById('sales-lists');
-    //create and add p element for the store name at the top of the list
-    var storeNameEl = document.createElement('p');
-    storeNameEl.appendChild(document.createTextNode(this.storeLocation));
-    salesListsDiv.appendChild(storeNameEl);
-    //create and add the ul element for the unlisted item list
-    var unlistedEl = document.createElement('ul');
-    var idAttributeName = `${this.storeLocation}-list`;
-    unlistedEl.setAttribute('id', idAttributeName);
-    unlistedEl.setAttribute('class', 'ulist');
-    salesListsDiv.appendChild(unlistedEl);
-    //create a pointer at the newly added ul element
-    var storeListUlEl = document.getElementById(idAttributeName);
-    for (var i = 0; i < this.cookiesPurchPerHour.length; i++)
-    {
-      var cookiesPurchInOneHour = this.cookiesPurchPerHour[i];
-      totalCookieSales += cookiesPurchInOneHour;
-      var oneHourCookieSalesListItem = document.createElement('li');
-      var currHour12HourTime = convert24To12HrTime(this.hoursOpen[i]);
-      oneHourCookieSalesListItem.appendChild(
-        document.createTextNode(`${currHour12HourTime}: ${cookiesPurchInOneHour.toFixed()} cookies`));
-      storeListUlEl.appendChild(oneHourCookieSalesListItem);
-    }
-    var totalCookieSalesListItem = document.createElement('li');
-    totalCookieSalesListItem.appendChild(document.createTextNode(`Total: ${totalCookieSales.toFixed()} cookies`));
-    storeListUlEl.appendChild(totalCookieSalesListItem);
+CookieStore.prototype.populateHoursOpen = function() {
+  const totalHoursOpen = Math.abs(closingTime24Hr - openingTime24Hr) / 100;
+  for (var i = 0; i <= totalHoursOpen; i++)
+  {
+    var hourTime = openingTime24Hr + (i * 100);
+    this.hoursOpen.push(hourTime);
   }
 };
 
-var tokyoStore = {
-  storeLocation: 'Tokyo',
-  minCustPerHour: 3,
-  maxCustPerHour: 24,
-  avgCookiesPerSale: 1.2,
-  hoursOpen: new Array (),
-  customersPerHour: new Array(),
-  cookiesPurchPerHour: new Array(),
-
-  populateHoursOpen: function() {
-    const totalHoursOpen = Math.abs(closingTime24Hr - openingTime24Hr) / 100;
-    for (var i = 0; i <= totalHoursOpen; i++)
-    {
-      var hourTime = openingTime24Hr + (i * 100);
-      this.hoursOpen.push(hourTime);
-    }
-  },
-
-  simulateCustPerHour: function() {
-    //populate hours open
-    this.populateHoursOpen();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      var customersInOneHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-      this.customersPerHour.push(customersInOneHour);
-    }
-  },
-
-  simulateCookiesPerHour: function() {
-    //populate customersPerHour
-    this.simulateCustPerHour();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      this.cookiesPurchPerHour.push(this.customersPerHour[i] * this.avgCookiesPerSale);
-    }
-  },
-
-  renderAsList: function() {
-    var totalCookieSales = 0;
-    var salesListsDiv = document.getElementById('sales-lists');
-    //create and add p element for the store name at the top of the list
-    var storeNameEl = document.createElement('p');
-    storeNameEl.appendChild(document.createTextNode(this.storeLocation));
-    salesListsDiv.appendChild(storeNameEl);
-    //create and add the ul element for the unlisted item list
-    var unlistedEl = document.createElement('ul');
-    var idAttributeName = `${this.storeLocation}-list`;
-    unlistedEl.setAttribute('id', idAttributeName);
-    unlistedEl.setAttribute('class', 'ulist');
-    salesListsDiv.appendChild(unlistedEl);
-    //create a pointer at the newly added ul element
-    var storeListUlEl = document.getElementById(idAttributeName);
-    for (var i = 0; i < this.cookiesPurchPerHour.length; i++)
-    {
-      var cookiesPurchInOneHour = this.cookiesPurchPerHour[i];
-      totalCookieSales += cookiesPurchInOneHour;
-      var oneHourCookieSalesListItem = document.createElement('li');
-      var currHour12HourTime = convert24To12HrTime(this.hoursOpen[i]);
-      oneHourCookieSalesListItem.appendChild(
-        document.createTextNode(`${currHour12HourTime}: ${cookiesPurchInOneHour.toFixed()} cookies`));
-      storeListUlEl.appendChild(oneHourCookieSalesListItem);
-    }
-    var totalCookieSalesListItem = document.createElement('li');
-    totalCookieSalesListItem.appendChild(document.createTextNode(`Total: ${totalCookieSales.toFixed()} cookies`));
-    storeListUlEl.appendChild(totalCookieSalesListItem);
+CookieStore.prototype.simulateCustPerHour = function() {
+  for (var i = 0; i < this.hoursOpen.length; i++)
+  {
+    var customersInOneHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+    this.customersPerHour.push(customersInOneHour);
   }
 };
 
-var dubaiStore = {
-  storeLocation: 'Dubai',
-  minCustPerHour: 11,
-  maxCustPerHour: 38,
-  avgCookiesPerSale: 3.7,
-  hoursOpen: new Array (),
-  customersPerHour: new Array(),
-  cookiesPurchPerHour: new Array(),
-
-  populateHoursOpen: function() {
-    const totalHoursOpen = Math.abs(closingTime24Hr - openingTime24Hr) / 100;
-    for (var i = 0; i <= totalHoursOpen; i++)
-    {
-      var hourTime = openingTime24Hr + (i * 100);
-      this.hoursOpen.push(hourTime);
-    }
-  },
-
-  simulateCustPerHour: function() {
-    //populate hours open
-    this.populateHoursOpen();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      var customersInOneHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-      this.customersPerHour.push(customersInOneHour);
-    }
-  },
-
-  simulateCookiesPerHour: function() {
-    //populate customersPerHour
-    this.simulateCustPerHour();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      this.cookiesPurchPerHour.push(this.customersPerHour[i] * this.avgCookiesPerSale);
-    }
-  },
-
-  renderAsList: function() {
-    var totalCookieSales = 0;
-    var salesListsDiv = document.getElementById('sales-lists');
-    //create and add p element for the store name at the top of the list
-    var storeNameEl = document.createElement('p');
-    storeNameEl.appendChild(document.createTextNode(this.storeLocation));
-    salesListsDiv.appendChild(storeNameEl);
-    //create and add the ul element for the unlisted item list
-    var unlistedEl = document.createElement('ul');
-    var idAttributeName = `${this.storeLocation}-list`;
-    unlistedEl.setAttribute('id', idAttributeName);
-    unlistedEl.setAttribute('class', 'ulist');
-    salesListsDiv.appendChild(unlistedEl);
-    //create a pointer at the newly added ul element
-    var storeListUlEl = document.getElementById(idAttributeName);
-    for (var i = 0; i < this.cookiesPurchPerHour.length; i++)
-    {
-      var cookiesPurchInOneHour = this.cookiesPurchPerHour[i];
-      totalCookieSales += cookiesPurchInOneHour;
-      var oneHourCookieSalesListItem = document.createElement('li');
-      var currHour12HourTime = convert24To12HrTime(this.hoursOpen[i]);
-      oneHourCookieSalesListItem.appendChild(
-        document.createTextNode(`${currHour12HourTime}: ${cookiesPurchInOneHour.toFixed()} cookies`));
-      storeListUlEl.appendChild(oneHourCookieSalesListItem);
-    }
-    var totalCookieSalesListItem = document.createElement('li');
-    totalCookieSalesListItem.appendChild(document.createTextNode(`Total: ${totalCookieSales.toFixed()} cookies`));
-    storeListUlEl.appendChild(totalCookieSalesListItem);
+CookieStore.prototype.simulateCookiesPerHour = function() {
+  for (var i = 0; i < this.hoursOpen.length; i++)
+  {
+    this.cookiesPurchPerHour.push(this.customersPerHour[i] * this.avgCookiesPerSale);
   }
 };
 
-var parisStore = {
-  storeLocation: 'Paris',
-  minCustPerHour: 20,
-  maxCustPerHour: 38,
-  avgCookiesPerSale: 2.3,
-  hoursOpen: new Array (),
-  customersPerHour: new Array(),
-  cookiesPurchPerHour: new Array(),
+CookieStore.prototype.renderAsList = function() {
+  var totalCookieSales = 0;
+  //set up object
+  this.populateHoursOpen();
+  this.simulateCustPerHour();
+  this.simulateCookiesPerHour();
 
-  populateHoursOpen: function() {
-    const totalHoursOpen = Math.abs(closingTime24Hr - openingTime24Hr) / 100;
-    for (var i = 0; i <= totalHoursOpen; i++)
-    {
-      var hourTime = openingTime24Hr + (i * 100);
-      this.hoursOpen.push(hourTime);
-    }
-  },
+  //select "sales-list" id, then create and add p element for the store name at the top of the list
+  var salesListsDiv = document.getElementById('sales-lists');
+  var storeNameEl = document.createElement('p');
+  storeNameEl.appendChild(document.createTextNode(this.storeLocation));
+  salesListsDiv.appendChild(storeNameEl);
 
-  simulateCustPerHour: function() {
-    //populate hours open
-    this.populateHoursOpen();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      var customersInOneHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-      this.customersPerHour.push(customersInOneHour);
-    }
-  },
+  //create and add the ul element for the unlisted item list
+  var unlistedEl = document.createElement('ul');
+  var idAttributeName = `${this.storeLocation}-list`;
+  unlistedEl.setAttribute('id', idAttributeName);
+  unlistedEl.setAttribute('class', 'ulist');
+  salesListsDiv.appendChild(unlistedEl);
 
-  simulateCookiesPerHour: function() {
-    //populate customersPerHour
-    this.simulateCustPerHour();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      this.cookiesPurchPerHour.push(this.customersPerHour[i] * this.avgCookiesPerSale);
-    }
-  },
+  //create a pointer at the newly added ul element and create list
+  var storeListUlEl = document.getElementById(idAttributeName);
+  for (var i = 0; i < this.cookiesPurchPerHour.length; i++)
+  {
+    var cookiesPurchInOneHour = this.cookiesPurchPerHour[i];
+    totalCookieSales += cookiesPurchInOneHour;
+    var oneHourCookieSalesListItem = document.createElement('li');
+    var currHour12HourTime = convert24To12HrTime(this.hoursOpen[i]);
 
-  renderAsList: function() {
-    var totalCookieSales = 0;
-    var salesListsDiv = document.getElementById('sales-lists');
-    //create and add p element for the store name at the top of the list
-    var storeNameEl = document.createElement('p');
-    storeNameEl.appendChild(document.createTextNode(this.storeLocation));
-    salesListsDiv.appendChild(storeNameEl);
-    //create and add the ul element for the unlisted item list
-    var unlistedEl = document.createElement('ul');
-    var idAttributeName = `${this.storeLocation}-list`;
-    unlistedEl.setAttribute('id', idAttributeName);
-    unlistedEl.setAttribute('class', 'ulist');
-    salesListsDiv.appendChild(unlistedEl);
-    //create a pointer at the newly added ul element
-    var storeListUlEl = document.getElementById(idAttributeName);
-    for (var i = 0; i < this.cookiesPurchPerHour.length; i++)
-    {
-      var cookiesPurchInOneHour = this.cookiesPurchPerHour[i];
-      totalCookieSales += cookiesPurchInOneHour;
-      var oneHourCookieSalesListItem = document.createElement('li');
-      var currHour12HourTime = convert24To12HrTime(this.hoursOpen[i]);
-      oneHourCookieSalesListItem.appendChild(
-        document.createTextNode(`${currHour12HourTime}: ${cookiesPurchInOneHour.toFixed()} cookies`));
-      storeListUlEl.appendChild(oneHourCookieSalesListItem);
-    }
-    var totalCookieSalesListItem = document.createElement('li');
-    totalCookieSalesListItem.appendChild(document.createTextNode(`Total: ${totalCookieSales.toFixed()} cookies`));
-    storeListUlEl.appendChild(totalCookieSalesListItem);
+    oneHourCookieSalesListItem.appendChild(
+      document.createTextNode(`${currHour12HourTime}: ${cookiesPurchInOneHour.toFixed()} cookies`));
+    storeListUlEl.appendChild(oneHourCookieSalesListItem);
   }
-};
 
-var limaStore = {
-  storeLocation: 'Lima',
-  minCustPerHour: 2,
-  maxCustPerHour: 16,
-  avgCookiesPerSale: 4.6,
-  hoursOpen: new Array (),
-  customersPerHour: new Array(),
-  cookiesPurchPerHour: new Array(),
-
-  populateHoursOpen: function() {
-    const totalHoursOpen = Math.abs(closingTime24Hr - openingTime24Hr) / 100;
-    for (var i = 0; i <= totalHoursOpen; i++)
-    {
-      var hourTime = openingTime24Hr + (i * 100);
-      this.hoursOpen.push(hourTime);
-    }
-  },
-
-  simulateCustPerHour: function() {
-    //populate hours open
-    this.populateHoursOpen();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      var customersInOneHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-      this.customersPerHour.push(customersInOneHour);
-    }
-  },
-
-  simulateCookiesPerHour: function() {
-    //populate customersPerHour
-    this.simulateCustPerHour();
-    for (var i = 0; i < this.hoursOpen.length; i++)
-    {
-      this.cookiesPurchPerHour.push(this.customersPerHour[i] * this.avgCookiesPerSale);
-    }
-  },
-
-  renderAsList: function() {
-    var totalCookieSales = 0;
-    var salesListsDiv = document.getElementById('sales-lists');
-    //create and add p element for the store name at the top of the list
-    var storeNameEl = document.createElement('p');
-    storeNameEl.appendChild(document.createTextNode(this.storeLocation));
-    salesListsDiv.appendChild(storeNameEl);
-    //create and add the ul element for the unlisted item list
-    var unlistedEl = document.createElement('ul');
-    var idAttributeName = `${this.storeLocation}-list`;
-    unlistedEl.setAttribute('id', idAttributeName);
-    unlistedEl.setAttribute('class', 'ulist');
-    salesListsDiv.appendChild(unlistedEl);
-    //create a pointer at the newly added ul element
-    var storeListUlEl = document.getElementById(idAttributeName);
-    for (var i = 0; i < this.cookiesPurchPerHour.length; i++)
-    {
-      var cookiesPurchInOneHour = this.cookiesPurchPerHour[i];
-      totalCookieSales += cookiesPurchInOneHour;
-      var oneHourCookieSalesListItem = document.createElement('li');
-      var currHour12HourTime = convert24To12HrTime(this.hoursOpen[i]);
-      oneHourCookieSalesListItem.appendChild(
-        document.createTextNode(`${currHour12HourTime}: ${cookiesPurchInOneHour.toFixed()} cookies`));
-      storeListUlEl.appendChild(oneHourCookieSalesListItem);
-    }
-    var totalCookieSalesListItem = document.createElement('li');
-    totalCookieSalesListItem.appendChild(document.createTextNode(`Total: ${totalCookieSales.toFixed()} cookies`));
-    storeListUlEl.appendChild(totalCookieSalesListItem);
-  }
+  //add a final li element to report on the total cookie sales
+  var totalCookieSalesListItem = document.createElement('li');
+  totalCookieSalesListItem.appendChild(document.createTextNode(`Total: ${totalCookieSales.toFixed()} cookies`));
+  storeListUlEl.appendChild(totalCookieSalesListItem);
 };
 
 //utility functions
@@ -369,18 +103,18 @@ function convert24To12HrTime(hourTime24Hour) {
   }
 }
 
-//exectuable
-seattleStore.simulateCookiesPerHour();
+//executable from constructors
+var seattleStore = new CookieStore('Seattle', 25, 65, 6.3);
 seattleStore.renderAsList();
 
-tokyoStore.simulateCookiesPerHour();
+var tokyoStore = new CookieStore('Tokyo', 3, 24, 1.2);
 tokyoStore.renderAsList();
 
-dubaiStore.simulateCookiesPerHour();
+var dubaiStore = new CookieStore('Dubai', 11, 38, 3.7);
 dubaiStore.renderAsList();
 
-parisStore.simulateCookiesPerHour();
+var parisStore = new CookieStore('Paris', 20, 38, 2.3);
 parisStore.renderAsList();
 
-limaStore.simulateCookiesPerHour();
+var limaStore = new CookieStore('Lima', 2, 16, 4.6);
 limaStore.renderAsList();
